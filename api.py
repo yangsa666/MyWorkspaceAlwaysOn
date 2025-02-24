96631d1eaf6e822daeec21c78853964b379dd604
Sa Yang <yangsa@microsoft.com>
2025-02-24 17:10:31 +0800

    init functions

---
+++ api.py
@@ -0,0 +1,67 @@
+import requests
+
+class Myworkspace:
+    def __init__(self, access_token):
+        self.headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Origin': 'https://myworkspace.microsoft.com',
+        'Referer': 'https://myworkspace.microsoft.com/',
+        'Authorization': f'Bearer {access_token}'
+        }
+        self.session = requests.Session()
+        self.session.headers.update(self.headers)
+
+    def get_workspaces(self):
+        try:
+            response = self.session.get("https://myworkspace-prod.trafficmanager.net/azureworkspace/userworkspaces")
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(f"Failed to get workspace, error: {error}")
+            return None
+    
+    def start_workspace(self, workplace_id):
+        try:
+            response = self.session.post(f"https://myworkspace-prod.trafficmanager.net/azureworkspace/start/{workplace_id}")
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(f"Failed to start workspace, error: {error}")
+            return None
+    
+    def extend_running_time(self, workplace_id, extend_hours):
+        try:
+            response = self.session.put(
+                f"https://myworkspace-prod.trafficmanager.net/azureworkspace/extendruntime/{workplace_id}",
+                data=str(extend_hours),
+                headers={
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json, text/plain, */*',
+                }
+            )
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(f"Failed to extend running time, error: {error}")
+            return None
+    
+    def get_nat_jit(self):
+        try:
+            response = self.session.get("https://myworkspace-prod.trafficmanager.net/natrule/jit")
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(f"Failed to get NAT JIT, error: {error}")
+            return None
+
+    def extend_jit(self, workplace_id, extend_hours):
+        try:
+            response = self.session.post(
+                "https://myworkspace-prod.trafficmanager.net/natrule/jit",
+                json={"WorkspaceID": workplace_id, "Hours": extend_hours}
+            )
+            response.raise_for_status()
+            return response.json()
+        except requests.exceptions.RequestException as error:
+            print(f"Failed to extend NAT JIT, error: {error}")
+            return None
---
+++ main.py
@@ -0,0 +1,77 @@
+import time
+from datetime import datetime, timezone
+from selenium import webdriver
+import json
+import pytz
+from tabulate import tabulate
+import logging
+from api import Myworkspace
+
+# initialize the browser to get token from session storage
+def get_token():
+    browser = webdriver.ChromiumEdge()
+    browser.get("https://myworkspace.microsoft.com/")
+    time.sleep(15)
+    storageKey = "be3c2eaf-1dce-4a89-bfc8-22d58fbc8a9f.72f988bf-86f1-41af-91ab-2d7cd011db47-login.windows.net-accesstoken-2ec0e21d-27e1-41a8-a154-6c791ce63b1a-72f988bf-86f1-41af-91ab-2d7cd011db47-api://2ec0e21d-27e1-41a8-a154-6c791ce63b1a/user_impersonation--"
+    sessionStorage = browser.execute_script("return window.sessionStorage")
+    tokenObj = sessionStorage[storageKey]
+    access_token = json.loads(tokenObj)['secret']
+    print('Got access_token:', access_token)
+    browser.quit()
+    return access_token
+
+def conver_jit_status(status_code):
+    if status_code == 5:
+        return "Activated"
+    elif status_code == 2:
+        return "Activating"
+    elif status_code == 4:
+        return "Deactivating"
+    else:
+        return "Not Activated"
+def convert_utc_to_shanghai(utc_datetime_str):
+    # Parse the UTC datetime string to a datetime object with timezone
+    utc_datetime = datetime.fromisoformat(utc_datetime_str)
+    # Define the Asia/Shanghai timezone
+    shanghai_timezone = pytz.timezone('Asia/Shanghai')
+    # Convert the UTC datetime to Asia/Shanghai datetime
+    shanghai_datetime = utc_datetime.astimezone(shanghai_timezone)
+    # Format the Asia/Shanghai datetime as a string
+    shanghai_datetime_str = shanghai_datetime.strftime('%Y-%m-%d %H:%M:%S.%f%z')
+    return shanghai_datetime_str
+
+def alwaysOn():
+    print("Starting AlwaysOn...")
+    token = get_token()
+    mw = Myworkspace(token)
+    workspaces = mw.get_workspaces()
+
+    workspace_table = list(map(lambda workspace: {'ID': workspace['ID'], 'Name': workspace['Name'], 'State': 'Running' if workspace['State'] == 4 else 'Stopped', 'ShutdownTime': convert_utc_to_shanghai(workspace['ShutdownTimeInUtc'])}, workspaces))
+    table = tabulate((workspace_table), headers='keys', tablefmt = "grid")
+    print(table)
+
+    nat_jit = mw.get_nat_jit()
+    jit_table = list(map(lambda jit: {'WorkspaceID': jit['WorkspaceID'], 'Status': conver_jit_status(jit['Status'])}, nat_jit))
+    table = tabulate((jit_table), headers='keys', tablefmt = "grid")
+    print(table)
+
+    for workspace in workspaces:
+        if workspace['State'] != 4:
+            print(f"Workspace {workspace['Name']} is stopped, starting workspace...")
+            mw.start_workspace(workspace['ID'])
+
+        if not any(jit['WorkspaceID'] == workspace['ID'] for jit in jit_table):
+            print(f"External Connectivity JIT not activated for workspace {workspace['Name']}, activating External Connectivity JIT...")
+            mw.extend_jit(workspace['ID'], 168)
+            print(f"External Connectivity JIT activated for workspace {workspace['Name']}")
+
+        left_running_mins = (datetime.fromisoformat(workspace['ShutdownTimeInUtc']) - datetime.now(timezone.utc)).total_seconds() / 60
+        if workspace['State'] == 4 and left_running_mins < 60 and left_running_mins > 0:
+            print(f"Workspace {workspace['Name']} is running, but running time has {left_running_mins} mins left, extending running time...")
+            result = mw.extend_running_time(workspace['ID'], 8)
+            print(f"Running time extended for workspace {workspace['Name']} for additional {result} hours")
+
+    print("Myworkspace function executed successfully")
+
+
+alwaysOn()
